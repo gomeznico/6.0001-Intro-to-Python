1b446f85e617e53bfc067dc20e82dee199b6efb1
Nico Gomez <nicolasgomez1994@gmail.com>
2024-09-25 17:57:04 -0400

    added complete pset 0

---
+++ Psets/Pset0/Pset0_Gomez.py
@@ -0,0 +1,11 @@
+# import numpy
+import math
+
+print('hello user')
+x = int(input('enter number x:'))
+y = int(input('enter number y:'))
+ans1 = int(math.pow(x,y))
+ans2 = int(math.log(x,2))
+
+print ("x^y=", ans1)
+print ('log(x)',ans2)
